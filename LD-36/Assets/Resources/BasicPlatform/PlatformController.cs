6a3885742655f7e6b1ae43d271846ca9282728db
Lucia Piváčková <pivackova.lucia@gmail.com>
2016-08-27 20:03:50 +0200

    add enemy + drop items (enemy, block)

---
+++ LD-36/Assets/Resources/BasicPlatform/PlatformController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+  public GameObject ItemObject;
+  public bool isBreakable;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if (collision.gameObject.layer == LayerMask.NameToLayer("Player") && isBreakable) {
+      die();
+    }
+    Debug.Log(collision.gameObject.layer + " " + LayerMask.NameToLayer("Player"));
+  }
+
+  private void die() {
+    Instantiate(ItemObject, transform.position, Quaternion.identity);
+    Destroy(gameObject);
+  }
+
+}
---
+++ LD-36/Assets/Resources/Enemy/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+  public GameObject ItemObject;
+
+  void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if (collision.gameObject.layer == LayerMask.NameToLayer("Item") && !isMoving(collision.gameObject)) {
+      die();
+    }
+  }
+
+  private bool isMoving(GameObject item) {
+    return (item.GetComponent<Rigidbody2D>().velocity.magnitude != 0);
+  }
+
+  private void die() {
+    Instantiate(ItemObject, transform.position, Quaternion.identity);
+    Destroy(gameObject);
+  }
+}
