1dbcba6d3b1a3f2c379703c32c050eee6cf7d71e
Lucia Piváčková <pivackova.lucia@gmail.com>
2016-08-27 18:14:15 +0200

    add item pickup

---
+++ LD-36/Assets/Resources/Item/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemController : MonoBehaviour {
+  private bool isCarried = false;
+  private Transform objectParent;
+
+  void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if (onLayer(collision.gameObject, "Player") && !isCarried) {
+      isCarried = true;
+      objectParent = transform.parent;
+      transform.SetParent(collision.GetComponent<PlayerController>().Holder);
+      transform.localPosition = Vector3.zero;
+    }
+
+    Debug.Log("enter" + collision.name);
+  }
+
+  /*private void OnTriggerExit2D(Collider2D collision) {
+    if (onLayer(collision.gameObject, "Player") && isCarried) {
+      isCarried = false;
+    }
+    transform.SetParent(objectParent);
+    Debug.Log("exit" + collision.name);
+  }*/
+
+  protected bool onLayer(GameObject go, string layer) {
+    return go.layer == LayerMask.NameToLayer(layer);
+  }
+}
---
+++ LD-36/Assets/Resources/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+  private Vector2 initPosition;
+  private GameObject Item = null;
+
+  public Transform Holder;
+
+	void Start () {
+    initPosition = transform.position;
+	}
+	
+	void Update () {
+	
+	}
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if (onLayer(collision.gameObject, "Item") && (Item == null)) {
+      Item = collision.gameObject;
+    }
+
+    Debug.Log("item++" + collision.name);
+  }
+
+  protected bool onLayer(GameObject go, string layer) {
+    return go.layer == LayerMask.NameToLayer(layer);
+  }
+
+}
