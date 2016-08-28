8b66347a9c09aebea2a67e3db920a559da84c533
Wryxo <strapko.martin@gmail.com>
2016-08-28 20:39:27 +0200

    Animations, Camera Follow

--- LD-36/Assets/PC2D/Example/CameraFollow.cs
+++ LD-36/Assets/PC2D/Example/CameraFollow.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace  PC2D
+namespace PC2D
 {
-    public class CameraFollow : MonoBehaviour
+  public class CameraFollow : MonoBehaviour
+  {
+    public Transform Target;
+    public float OffsetX;
+    public float OffsetY;
+
+    // Update is called once per frame
+    void Update()
     {
-        public Transform target;
-
-        // Update is called once per frame
-        void Update()
-        {
-            Vector3 pos = transform.position;
-            pos.x = target.position.x;
-            pos.y = target.position.y;
-
-            transform.position = pos;
-        }
+      if (!Target) {
+        Debug.Log("NIKOHO NEMAM!");
+        return;
+      }
+      Vector3 pos = transform.position;
+      if (Mathf.Abs(Target.position.x - transform.position.x) > OffsetX)
+      {
+        pos.x = Target.position.x - ((Target.position.x > transform.position.x) ? OffsetX : -OffsetX);
+      }
+      if (Mathf.Abs(Target.position.y - transform.position.y) > OffsetY)
+      {
+        pos.y = Target.position.y - ((Target.position.y > transform.position.y) ? OffsetY : -OffsetY);
+      }
+      pos.x = Mathf.Clamp(pos.x, -21.0f, 26.2f);
+      pos.y = Mathf.Clamp(pos.y, -6.0f, 12.9f);
+      transform.position = pos;
     }
+  }
 }
--- LD-36/Assets/Resources/Spawner/SpawnController.cs
+++ LD-36/Assets/Resources/Spawner/SpawnController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnController : MonoBehaviour {
   public GameObject[] parentObjects = new GameObject[3];
@@ -7,7 +8,17 @@ public class SpawnController : MonoBehaviour {
   public int[] maxCount = new int[3];
   private int[] oldCount = new int[3];
 
-  void Awake() {
+  void Start() {
+    for (int i = 0; i < 5; i++) { 
+      for (int j = 0; j < objects.Length; j++)
+      {
+        int roll = Random.Range(0, 100);
+        if (roll < 220 - (30 * parentObjects[j].transform.childCount))
+        {
+          spawnObject(j, getRandomSpawn(true));
+        }
+      }
+    }
     setChildrenCount();
   }
 
@@ -18,30 +29,57 @@ public class SpawnController : MonoBehaviour {
 
 
   private void setChildrenCount() {
-    for (int i = 0; i < oldCount.Length; i++) {
-      oldCount[i] = parentObjects[i].transform.childCount;
+    for (int i = 0; i < oldCount.Length - 1; i++) {
+      oldCount[i] = parentObjects[i].transform.childCount + GameObject.FindGameObjectsWithTag("HamsterItem").Length;
     }
+    oldCount[2] = parentObjects[2].transform.childCount;
   }
+
   private void respawn() {
-    for (int i = 0; i < objects.Length; i++) {
-      if (parentObjects[i].transform.childCount < oldCount[i]) {
-        for (int j = 0; j < objects.Length; j++) {
-          spawnObject(j, getRandomSpawn());
+    for (int i = 0; i < objects.Length-1; i++) {
+      if (parentObjects[i].transform.childCount + GameObject.FindGameObjectsWithTag("HamsterItem").Length < oldCount[i]) {
+        for (int j = 0; j < objects.Length-1; j++) {
+          int roll = Random.Range(0, 100);
+          if (roll < 220 - 30 * (parentObjects[j].transform.childCount + GameObject.FindGameObjectsWithTag("HamsterItem").Length))
+          {
+            spawnObject(j, getRandomSpawn(false));
+          }
         }
       }
     }
+    if (parentObjects[2].transform.childCount < oldCount[2])
+    {
+        int roll = Random.Range(0, 100);
+        if (roll < 220 - (30 * parentObjects[2].transform.childCount))
+        {
+          spawnObject(2, getRandomSpawn(false));
+        }
+    }
   }
 
-  private Vector3 getRandomSpawn() {
-    int spawn = Random.Range(0, transform.childCount);
+  private Vector3 getRandomSpawn(bool first) {
+    List<int> candidates = new List<int>();
+    for (int i = 0; i < transform.childCount; i++)
+    {
+      if (!first) { 
+        if (transform.GetChild(i).GetComponent<Renderer>().isVisible)
+        {
+          candidates.Add(i);
+        }
+      } else
+      {
+        candidates.Add(i);
+      }
+    }
+    int spawn = Random.Range(0, candidates.Count);
     return transform.GetChild(spawn).transform.position;
   }
 
   private void spawnObject(int index, Vector3 spawnPosition) {
-    if (!(parentObjects[index].transform.childCount >= maxCount[index])) {
+    //if (!(parentObjects[index].transform.childCount >= maxCount[index])) {
       GameObject ego = Instantiate(objects[index], parentObjects[index].transform) as GameObject;
       ego.transform.position = spawnPosition;
-    }
+    //}
     
   }
 }
