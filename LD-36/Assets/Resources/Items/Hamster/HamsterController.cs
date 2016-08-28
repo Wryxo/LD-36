7e43df2667b857da2380a3f87a71c0dbfcc5238a
Lucia Piváčková <pivackova.lucia@gmail.com>
2016-08-28 12:11:06 +0200

    hamster fix

--- LD-36/Assets/Resources/Items/Hamster/HamsterController.cs
+++ LD-36/Assets/Resources/Items/Hamster/HamsterController.cs
@@ -10,13 +10,15 @@ public class HamsterController : MonoBehaviour {
 
   private void OnCollisionEnter2D(Collision2D collision) {
     if (collision.gameObject.layer == LayerMask.NameToLayer("Floor") && wasThrown && !wakingUp) {
+      wakingUp = true;
       Invoke("wakeUp", WakeUpTimer);      
     }
   }
 
   private void wakeUp() {
-    wakingUp = true;
-    Instantiate(HamsterObject, transform.position, Quaternion.identity);
-    Destroy(gameObject);
+    if (!(transform.parent != null && transform.parent.name.Contains("Hodor"))) {
+      Instantiate(HamsterObject, transform.position, Quaternion.identity);
+      Destroy(gameObject);
+    }    
   }
 }
