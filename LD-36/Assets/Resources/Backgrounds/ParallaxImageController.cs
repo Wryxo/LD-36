ef9b21b1f9d9f62cbc425dd4dffa08780b268f60
Kubo Motyl <motyl.kubo@gmail.com>
2016-08-29 05:43:16 +0200

    add parallaxing

---
+++ LD-36/Assets/Resources/Backgrounds/ParallaxImageController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class ParallaxImageController : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D collider) {
+		transform.parent.GetComponent<ScrollingParallaxController>().TriggerCollisionWith(gameObject);
+	}
+}
---
+++ LD-36/Assets/Resources/Backgrounds/ScrollingParallaxController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ScrollingParallaxController : MonoBehaviour {
+	public GameObject Engine;
+	public GameObject[] Images;
+
+	public void TriggerCollisionWith(GameObject go) {
+		if (go.GetInstanceID() == Images[0].GetInstanceID()) {
+			Images[2].transform.position = new Vector3(
+				Images[0].transform.position.x - Images[2].GetComponent<SpriteRenderer>().bounds.extents.x * 2,
+				Images[2].transform.position.y,
+				Images[2].transform.position.z
+			);
+
+			GameObject temp = Images[2];
+			Images[2] = Images[1];
+			Images[1] = Images[0];
+			Images[0] = temp;
+		} else if (go.GetInstanceID() == Images[2].GetInstanceID()) {
+			Images[0].transform.position = new Vector3(
+				Images[2].transform.position.x + Images[0].GetComponent<SpriteRenderer>().bounds.extents.x * 2,
+				Images[0].transform.position.y,
+				Images[0].transform.position.z
+			);
+
+			GameObject temp = Images[0];
+			Images[0] = Images[1];
+			Images[1] = Images[2];
+			Images[2] = temp;
+		}
+	}
+}
