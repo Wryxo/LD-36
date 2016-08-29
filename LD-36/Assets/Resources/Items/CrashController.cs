d4658fe5df3748459c8aafdf558b80e67c271b44
Kubo Motyl <motyl.kubo@gmail.com>
2016-08-29 16:41:52 +0200

    add crash dust particle effect

---
+++ LD-36/Assets/Resources/Items/CrashController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CrashController : MonoBehaviour {
+	public GameObject Particles;
+	public int ParticleCount;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Player" || collision.gameObject.layer == LayerMask.NameToLayer("Item"))
+			return;
+
+		Vector2 collisionWeighted = collision.contacts[0].point;
+		foreach (ContactPoint2D colPoint in collision.contacts) {
+			collisionWeighted = (collisionWeighted + colPoint.point) / 2f;
+		}
+		collisionWeighted -= new Vector2(transform.position.x, transform.position.y);
+
+		Vector3 emitVector = collisionWeighted.normalized * 360f;
+		emitVector = new Vector3(emitVector.y, emitVector.x, emitVector.z);
+		Particles.transform.localEulerAngles = emitVector;
+
+		Particles.GetComponent<ParticleSystem>().Emit(ParticleCount);
+	}
+}
